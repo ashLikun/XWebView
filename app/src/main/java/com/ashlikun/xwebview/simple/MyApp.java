13a4332b5124b5c2c2fe25963bb69f9507fd9f4f
likun <496546144@qq.com>
2022-06-21 10:14:00 +0800

    优化

--- app/src/main/java/com/ashlikun/xwebview/simple/MyApp.java
+++ app/src/main/java/com/ashlikun/xwebview/simple/MyApp.java
@@ -3,7 +3,10 @@ package com.ashlikun.xwebview.simple;
 import android.app.Application;
 import android.util.Log;
 
+import com.tencent.smtt.export.external.TbsCoreSettings;
 import com.tencent.smtt.sdk.QbSdk;
+
+import java.util.HashMap;
 
 /**
  * 作者　　: 李坤
@@ -16,12 +19,16 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // 初始化X5内核时候的配置
+        HashMap map = new HashMap<>();
+        map.put(TbsCoreSettings.TBS_SETTINGS_USE_SPEEDY_CLASSLOADER, true);
+        map.put(TbsCoreSettings.TBS_SETTINGS_USE_DEXLOADER_SERVICE, true);
+        QbSdk.initTbsSettings(map);
         QbSdk.initX5Environment(this, new QbSdk.PreInitCallback() {
             @Override
             public void onCoreInitFinished() {
                 Log.e("QbSdk", "内核加载成功");
             }
-
             @Override
             public void onViewInitFinished(boolean b) {
                 Log.e("QbSdk", "加载内核是否成功:" + b);
